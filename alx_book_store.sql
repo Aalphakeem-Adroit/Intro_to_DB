e1832a1c92f3d367a916e0071b4dff8291cfe284
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-08-17 07:22:42 +0100

    Creating Book Table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,3 +4,11 @@ CREATE DATABASE alx_book_store;
 -- Using the Database
 USE alx_book_store;
 
+-- Creating Books Table
+CREATE TABLE Books (
+    book_id PRIMARY KEY,
+    title VARCHAR(50),
+    FOREIGN_KEY (author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+)
